072615a378de787241fce17ca332a34f5c1f5871
YuriiSelebynka <selebinkayuriy@gmail.com>
2021-06-22 00:08:33 +0300

    for education

--- src/main/java/com/yuriiselebynka/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
+++ src/main/java/com/yuriiselebynka/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
@@ -18,7 +18,7 @@ public class EmployeeDAOImpl implements EmployeeDAO{
     private SessionFactory sessionFactory;
 
     @Override
-    @Transactional
+
     public List<Employee> getAllEmployees() {
 
         Session session = sessionFactory.getCurrentSession();
@@ -29,5 +29,30 @@ public class EmployeeDAOImpl implements EmployeeDAO{
         List<Employee> allEmployees = query.getResultList();
 
         return allEmployees;
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+
+        Session session = sessionFactory.getCurrentSession();
+
+        session.saveOrUpdate(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+
+        Employee employee = session.get(Employee.class, id);
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Employee> query = session.createQuery("delete from Employee " +
+                "where id =:employeeId");
+        query.setParameter("employeeId", id);
+        query.executeUpdate();
     }
 }
